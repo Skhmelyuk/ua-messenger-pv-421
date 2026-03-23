4c980095dcffe77cdf6de35d758181b98e0df2e8
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-03-23 16:30:05 +0200

    lesson-03

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,15 +2,24 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    users: defineTable({
-        username: v.string(),
-        fullname: v.string(),
-        email: v.string(),
-        bio: v.optional(v.string()),
-        image: v.string(),
-        followers: v.number(),
-        following: v.number(),
-        posts: v.number(),
-        clerkId: v.string(),
-    }).index("by_clerk_id", ["clerkId"]),
+  users: defineTable({
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    followers: v.number(),
+    following: v.number(),
+    posts: v.number(),
+    clerkId: v.string(),
+  }).index("by_clerk_id", ["clerkId"]),
+
+  posts: defineTable({
+    userId: v.id("users"),
+    imageUrl: v.string(),
+    storageId: v.id("_storage"),
+    caption: v.optional(v.string()),
+    likes: v.number(),
+    comments: v.number(),
+  }).index("by_user", ["userId"]),
 });
--- convex/users.ts
+++ convex/users.ts
@@ -1,33 +1,46 @@
-import { mutation } from "./_generated/server";
+import { mutation, QueryCtx, MutationCtx } from "./_generated/server";
 import { v } from "convex/values";
 
+export const getAuthenticatedUser = async (ctx: QueryCtx | MutationCtx) => {
+  const identity = await ctx.auth.getUserIdentity();
+  if (!identity) throw new Error("Unauthorized");
+
+  const currentUser = await ctx.db
+    .query("users")
+    .withIndex("by_clerk_id", (q) => q.eq("clerkId", identity.subject))
+    .first();
+  if (!currentUser) throw new Error("User not found");
+
+  return currentUser;
+};
+
 export const createUser = mutation({
-    args: {
-        username: v.string(),
-        fullname: v.string(),
-        email: v.string(),
-        bio: v.optional(v.string()),
-        image: v.string(),
-        clerkId: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const existingUser = await ctx.db
-            .query("users")
-            .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
-            .first();
+  args: {
+    username: v.string(),
+    fullname: v.string(),
+    email: v.string(),
+    bio: v.optional(v.string()),
+    image: v.string(),
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const existingUser = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .first();
 
-        if (existingUser) return;
+    if (existingUser) return;
 
-        await ctx.db.insert("users", {
-            username: args.username,
-            fullname: args.fullname,
-            email: args.email,
-            bio: args.bio,
-            image: args.image,
-            clerkId: args.clerkId,
-            followers: 0,
-            following: 0,
-            posts: 0,
-        });
-    },
+    await ctx.db.insert("users", {
+      username: args.username,
+      fullname: args.fullname,
+      email: args.email,
+      bio: args.bio,
+      image: args.image,
+      clerkId: args.clerkId,
+      followers: 0,
+      following: 0,
+      posts: 0,
+    });
+  },
 });
